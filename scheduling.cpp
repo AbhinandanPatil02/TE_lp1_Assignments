39f18409b9d54b2f56d4e4519cd32805634505c5
Abhinandan Patil <166230074+AbhinandanPatil02@users.noreply.github.com>
2024-11-10 16:28:00 +0530

    Update scheduling.cpp

--- scheduling.cpp
+++ scheduling.cpp
@@ -1,217 +1,259 @@
-#include <climits>
-#include <iomanip>
-#include <iostream>
+#include<iostream>
+#include<climits>
 using namespace std;
 
-class Process {
-public:
-  int id;
-  int at;
-  int bt;
-  int priority;
-  int remaining_bt;
+
+class process{
+    public:
+    int pid;
+    int bt;
+    int at;
+    int priority;
+    int remaining_bt;
+
 };
 
-class SchedulingAlgo {
-  Process *p;
-
-public:
-  SchedulingAlgo(Process *q) { p = q; }
-
-  void FCFS_Calc(int n, int ct[], int tat[], int wt[]) {
-    int wait = 0;
-    for (int i = 0; i < n; i++) {
-      ct[i] = wait + p[i].bt;
-      tat[i] = ct[i] - p[i].at;
-      wt[i] = tat[i] - p[i].bt;
-      wait = ct[i];
+class algo{
+    public:
+    process *p;
+    algo(process*q){
+        this->p=q;
     }
-  }
-
-  void SJF_PreEmp(int n, int ct[], int tat[], int wt[]) {
-    int completed = 0;
-    int current_time = 0;
-
-    while (completed < n) {
-      int min_bt = INT_MAX;
-      int min_bt_index = -1;
-      for (int i = 0; i < n; i++) {
-        if (p[i].at <= current_time && p[i].remaining_bt < min_bt &&
-            p[i].remaining_bt > 0) {
-          min_bt = p[i].remaining_bt;
-          min_bt_index = i;
+    void fcfs(int n,int ct[],int tat[],int wt[]){
+        int wait=0;
+        for(int i=0;i<n;i++){
+            if(wait<p[i].at){
+                wait=p[i].at;
+            }
+            ct[i]=wait+p[i].bt;
+            tat[i]=ct[i]-p[i].at;
+            wt[i]=tat[i]-p[i].bt;
+            wait=ct[i];
         }
-      }
-
-      if (min_bt_index == -1) {
-        current_time++;
-        continue;
-      }
-
-      p[min_bt_index].remaining_bt--;
-      current_time++;
-
-      if (p[min_bt_index].remaining_bt == 0) {
-        completed++;
-        ct[min_bt_index] = current_time;
-        tat[min_bt_index] = ct[min_bt_index] - p[min_bt_index].at;
-        wt[min_bt_index] = tat[min_bt_index] - p[min_bt_index].bt;
-      }
     }
-  }
 
-  void priority_NonPreEmp(int n, int ct[], int tat[], int wt[]) {
-    int completed = 0;
-    int current_time = 0;
 
-    while (completed < n) {
-      int highest_priority_index = -1;
-      int highest_priority = INT_MAX;
-
-      for (int i = 0; i < n; i++) {
-        if (p[i].at <= current_time && p[i].priority < highest_priority &&
-            p[i].remaining_bt > 0) {
-          highest_priority = p[i].priority;
-          highest_priority_index = i;
+    void sjfpre(int n,int ct[],int tat[],int wt[]){
+        int completed=0;
+        int current_time=0;
+        while(completed<n){
+            int min_bt=INT_MAX;
+            int min_bt_index=-1;
+            for(int i=0;i<n;i++){
+                if(p[i].at<=current_time&&p[i].remaining_bt<min_bt&&p[i].remaining_bt>0){
+                    min_bt=p[i].remaining_bt;
+                    min_bt_index=i;
+                }
+            }
+            if(min_bt_index==-1){
+                current_time++;
+                continue;
+            }
+            p[min_bt_index].remaining_bt--;
+            current_time++;
+            if(p[min_bt_index].remaining_bt==0){
+                completed++;
+                ct[min_bt_index]=current_time;
+                tat[min_bt_index]=ct[min_bt_index]-p[min_bt_index].at;
+                wt[min_bt_index]=tat[min_bt_index]-p[min_bt_index].bt;
+            }
         }
-      }
-
-      if (highest_priority_index != -1) {
-        ct[highest_priority_index] =
-            current_time + p[highest_priority_index].bt;
-        tat[highest_priority_index] =
-            ct[highest_priority_index] - p[highest_priority_index].at;
-        wt[highest_priority_index] =
-            tat[highest_priority_index] - p[highest_priority_index].bt;
-
-        p[highest_priority_index].remaining_bt = 0;
-        completed++;
-        current_time = ct[highest_priority_index];
-      } else {
-        current_time++;
-      }
-    }
-  }
-
-  void RoundRobin_PreEmp(int n, int ct[], int tat[], int wt[], int quantum) {
-    int remaining_processes = n;
-    int current_time = 0;
-    int *remaining_time = new int[n];
-
-    for (int i = 0; i < n; i++) {
-      remaining_time[i] = p[i].bt;
     }
 
-    while (remaining_processes > 0) {
-      bool done = true;
 
-      for (int i = 0; i < n; i++) {
-        if (p[i].at <= current_time && remaining_time[i] > 0) {
-
-          done = false; // There is still a pending process
-
-          if (remaining_time[i] > quantum) {
-            current_time += quantum;
-            remaining_time[i] -= quantum;
-          } else {
-            current_time += remaining_time[i];
-            ct[i] = current_time;
-            tat[i] = ct[i] - p[i].at;
-            wt[i] = tat[i] - p[i].bt;
-            remaining_processes--;
-            remaining_time[i] = 0;
-          }
+    void prioritynon(int n,int ct[],int tat[],int wt[]){
+        int completed=0;
+        int current_time=0;
+        // lower the no higher the priority
+        while(n>completed){
+            int highest_priority=INT_MAX;
+            int highest_priority_index=-1;
+            for(int i=0;i<n;i++){
+                if(p[i].at<=current_time&&p[i].priority<highest_priority&&p[i].remaining_bt>0){
+                    highest_priority=p[i].priority;
+                    highest_priority_index=i;
+                }
+            }
+            if(highest_priority_index!=-1){
+                completed++;
+                ct[highest_priority_index]=current_time+p[highest_priority_index].bt;
+                tat[highest_priority_index]=ct[highest_priority_index]-p[highest_priority_index].at;
+                wt[highest_priority_index]=tat[highest_priority_index]-p[highest_priority_index].bt;
+                p[highest_priority_index].remaining_bt=0;
+                current_time=ct[highest_priority_index];
+            }
+            else{
+                current_time++;
+            }
+            
         }
-      }
 
-      if (done) {
-        current_time++; // No pending process, move to the next time unit
-      }
     }
-    delete[] remaining_time;
-  }
+
+
+    void rr(int n,int ct[],int tat[],int wt[],int qunt){
+        int remaining=n;
+        int rem[n];
+        int current_time=0;
+        for(int i=0;i<n;i++){
+            rem[i]=p[i].bt;
+        }
+        while (remaining>0){
+            int done=1;
+            for(int i=0;i<n;i++){
+                if(p[i].at<=current_time&&rem[i]>0){
+                    done=0;
+                    if(qunt<rem[i]){
+                        current_time+=qunt;
+                        rem[i]-=qunt;
+                    }
+                    else{
+                        current_time=rem[i];
+                        remaining--;
+                        rem[i]=0;
+                        ct[i]=current_time;
+                        tat[i]=ct[i]-p[i].at;
+                        wt[i]=tat[i]-p[i].bt;
+                    }
+
+                }
+            }
+            if(done){
+                current_time++;
+            }
+            
+        }
+        
+
+
+    }
+
+
+
+
+
+
+
+// n,ct,tat,wt
+    void print(int n,int ct[],int tat[],int wt[]){
+        cout<<"p_id\tat\tbt\tct\ttat\twt"<<endl;
+        for(int i=0;i<n;i++){
+            cout<<p[i].pid<<"\t"<<p[i].at<<"\t"<<p[i].bt<<"\t"<<ct[i]<<"\t"<<tat[i]<<"\t"<<wt[i]<<endl;
+        }
+    }
+
+
+
+
+
+
+
+
+
 };
 
-void displayResults(Process p[], int n, int ct[], int tat[], int wt[]) {
-  cout << "ProgId" << setw(8) << "AT" << setw(8) << "BT" << setw(8) << "CT"
-       << setw(8) << "TAT" << setw(8) << "WT" << endl;
-  for (int i = 0; i < n; i++) {
-    cout << p[i].id << setw(8) << p[i].at << setw(8) << p[i].bt << setw(8)
-         << ct[i] << setw(8) << tat[i] << setw(8) << wt[i] << endl;
-  }
+
+
+
+
+
+
+
+
+int main(){
+
+    process *p;
+    int n;
+    cout<<"Ente no  of process : ";
+    cin>>n;
+    p=new process[n];
+    algo a(p);
+
+    int wt[n];
+    int tat[n];
+    int ct[n];
+
+    for(int i=0;i<n;i++){
+        cout<<"Enter p_id : ";
+        cin>>p[i].pid;
+        cout<<"Enter Arival Time : ";
+        cin>>p[i].at;
+        cout<<"Enter Brust Time : ";
+        cin>>p[i].bt;
+        p[i].remaining_bt=p[i].bt;
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<"Enter Priprity : ";
+        cin>>p[i].priority;
+    }
+
+
+
+    // a.fcfs(n,ct,tat,wt);
+    // a.sjfpre(n,ct,tat,wt);
+    a.prioritynon(n,ct,tat,wt);
+
+    cout<<endl;
+    a.print(n,ct,tat,wt);
+
+
+    return 0;
 }
 
-int main() {
-  Process *p;
-  int n;
-  cout << "Enter the number of processes: ";
-  cin >> n;
-  p = new Process[n];
 
-  int choice, quantum;
-  do {
-    cout << "\nSelect Scheduling Algorithm:\n";
-    cout << "1. First Come First Serve (FCFS)\n";
-    cout << "2. Shortest Job First (SJF) Preemptive\n";
-    cout << "3. Priority Scheduling Non-Preemptive\n";
-    cout << "4. Round Robin Scheduling\n";
-    cout << "5. Exit\n";
-    cout << "Enter your choice: ";
-    cin >> choice;
 
-    cout << "Enter data (ProgId AT BT):" << endl;
-    for (int i = 0; i < n; i++) {
-      cin >> p[i].id >> p[i].at >> p[i].bt;
-      p[i].remaining_bt = p[i].bt;
-    }
 
-    if (choice == 3) {
-      cout << "Enter Priority for each process:" << endl;
-      for (int i = 0; i < n; i++) {
-        cin >> p[i].priority;
-      }
-    }
 
-    int ct[n], tat[n], wt[n];
 
-    SchedulingAlgo algo(p);
 
-    switch (choice) {
-    case 1:
-      algo.FCFS_Calc(n, ct, tat, wt);
-      cout << "First Come First Serve (FCFS) Results:\n";
-      displayResults(p, n, ct, tat, wt);
-      break;
 
-    case 2:
-      algo.SJF_PreEmp(n, ct, tat, wt);
-      cout << "Shortest Job First (SJF) Preemptive Results:\n";
-      displayResults(p, n, ct, tat, wt);
-      break;
 
-    case 3:
-      algo.priority_NonPreEmp(n, ct, tat, wt);
-      cout << "Priority Scheduling Non-Preemptive Results:\n";
-      displayResults(p, n, ct, tat, wt);
-      break;
 
-    case 4:
-      cout << "Enter quantum time: ";
-      cin >> quantum;
-      algo.RoundRobin_PreEmp(n, ct, tat, wt, quantum);
-      cout << "Round Robin Scheduling Results:\n";
-      displayResults(p, n, ct, tat, wt);
-      break;
 
-    case 5:
-      cout << "Exiting...\n";
-      break;
 
-    default:
-      cout << "Invalid choice! Please try again.\n";
-    }
-  } while (choice != 5);
+// 4  1 0 2  2 1 2  3 5 3  4 6 4
 
-  delete[] p;
-  return 0;
-}
+// process id       Arival time    Brust time      Completion time TurnAround Time Waiting Time
+// 1               0               2               2               2               0
+// 2               1               2               4               3               1
+// 3               5               3               8               3               0
+// 4               6               4               12              6               2
+
+
+//------------------------------------------------------------------------------------------------
+
+// 4   1 0 5  2 1 3  3 2 4  4 4 1
+
+
+// process id       Arival time    Brust time      Completion time TurnAround Time Waiting Time
+// 1               0               5               9               9               4
+// 2               1               3               4               3               0
+// 3               2               4               13              11              7
+// 4               4               1               5               1               0
+
+//------------------------------------------------------------------------------------------------
+
+
+
+// 4   1 0 5  2 1 3  3 2 4  4 4 1
+//     5 3 2 1 4
+// process id       Arival time    Brust time      Completion time TurnAround Time Waiting Time
+// 1               0               5               5               5               0
+// 2               1               3               13              12              9
+// 3               2               4               10              8               4
+// 4               4               1               6               2               1
+
+// -------------------------------------------------------------------------------------------------
+// 4   1 0 5  2 1 4  3 2 2  4 4 1             2
+
+
+// process id       Arival time    Brust time      Completion time TurnAround Time Waiting Time
+// 1               0               5               12              12              7
+// 2               1               4               11              10              6
+// 3               2               2               6               4               2
+// 4               4               1               7               3               2
+
+
+
+
